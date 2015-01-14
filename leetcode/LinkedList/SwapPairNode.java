b4d6f49f7cfa51d2212a6056e45064e7b2bc4d9b
startupjing <lvjing111@gmail.com>
2015-01-14 01:03:26 -0600

    finish list problems

--- leetcode/LinkedList/SwapPairNode.java
+++ leetcode/LinkedList/SwapPairNode.java
@@ -1,34 +1,30 @@
 //swap adjacent pair nodes
 
-
+//sol: two pointers, one points to the node before current pair, another points to 
+//the first node in the pair; swap continues if two nodes in current pair are not null
 public class SwapPairNode{
 	public ListNode swapPairs(ListNode head){
-		if(head == null){
-			return null;
-		}else if(head.next == null){
+		if(head==null || head.next==null){
 			return head;
 		}
-
-		//result list
-		ListNode res = head;
-		ListNode resCurr = ;
-		//pointers to adjacent nodes
-		ListNode prev = head;
-		ListNode nxt = head.next;
-
-		while(prev!=null && next!=null){
-			//swap
-			prev.next = nxt.next;
-			nxt.next = prev;
-			//first time swapping
-			if(res == null){
-				res = nxt;
-
-
-
-
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		//previous node before a pair
+		ListNode pairHead = dummy;
+		//first node in pair
+		ListNode pair = dummy.next;
+        //continue if both nodes in the pair are not null
+		while(pair!=null && pair.next!=null){
+			//store first node in next pair
+			ListNode temp = pair.next.next;
+			//swap nodes in current pair
+			pair.next.next = pairHead.next;
+			pairHead.next = pair.next;
+			pair.next = temp;
+			//prepare for next pair
+			pairHead = pair;
+			pair = pairHead.next;
 		}
-
-
+		return dummy.next;
 	}
 }
